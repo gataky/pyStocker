b2404f4d4ff3c176df4431ce71a85abf98b20bd0
Jeff Gregory <JeffMGreg@gmail.com>
2012-09-17 14:56:13 -0700

    converts string types to floats

--- trending.py
+++ trending.py
@@ -1,5 +1,5 @@
 
-def trending(data, reversal):
+def trender(data, reversal):
 
     def initialize(point, previous):
         delta = point - previous
@@ -7,37 +7,39 @@ def trending(data, reversal):
             return True
         else:
             return False
-    
+
+    data  = map(lambda x: float(x), data)
+
     peak  = min(data)
     dip   = max(data)
     trend = []
     for i, point in enumerate(data[1:]):
-        
+
         if i == 0:
             trending = initialize(point, data[i])
             trend.extend((trending, trending))
             continue
-        
+
         delta = point - data[i]
-        
+
         if trending and delta > 0:
             if point > peak:
                 peak = point
-        
+
         elif trending and delta < 0:
             if abs(peak - point) >= reversal:
                 trending = not trending
                 dip = point
-        
+
         elif not trending and delta > 0:
             if abs(dip - point) >= reversal:
                 trending = not trending
                 peak = point
-        
+
         elif not trending and delta < 0:
             if point < dip:
                 dip = point
-                
+
         trend.append(trending)
-    
+
     return trend
